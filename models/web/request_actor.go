10bdb9cbec5a634e2a9c2d6ee3f34460f8671384
khilmi-aminudin <khilmiaminudin715@gmail.com>
2022-01-26 15:54:53 +0700

    modify request actor and user

--- models/web/request_actor.go
+++ models/web/request_actor.go
@@ -4,3 +4,9 @@ type RequestCreateActor struct {
 	FirstName string `json:"first_name" validate:"required,min=3,max=200"`
 	LastName  string `json:"last_name" validate:"required,min=3,max=200"`
 }
+
+type RequestUpdateActor struct {
+	ActorId   int64  `json:"actor_id" validate:"required"`
+	FirstName string `json:"first_name" validate:"required,min=3,max=200"`
+	LastName  string `json:"last_name" validate:"required,min=3,max=200"`
+}
--- models/web/request_user.go
+++ models/web/request_user.go
@@ -4,3 +4,9 @@ type RequestCreateUser struct {
 	Username string `validate:"required,min=3" json:"username"`
 	Passowrd string `validate:"required,min=8" json:"password"`
 }
+
+type RequestUpdateUser struct {
+	UserId   int64  `json:"user_id"`
+	Username string `validate:"required,min=3" json:"username"`
+	Passowrd string `validate:"required,min=8" json:"password"`
+}
